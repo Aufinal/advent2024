dc7a0f0eecb9c7cb812f151c56a2edd6ffb689c8
Ludovic Stephan <ludovic.stephan@protonmail.com>
2024-12-22 23:56:56 +0100

    Day 21

--- 21.jl
+++ 21.jl
@@ -11,63 +11,40 @@ const coords_arr = Dict([
     'A' => (2, 1)
 ])
 
-const dir_arr = Dict([
-    '^' => (0, 1),
-    '>' => (1, 0),
-    '<' => (-1, 0),
-    'v' => (0, -1),
-])
+const dx_dict = Dict([-1 => '<', 0 => 'A', 1 => '>'])
+const dy_dict = Dict([-1 => 'v', 0 => 'A', 1 => '^'])
 
-function move_to(p1, p2)
+function shortest(p1, p2, best_paths, forbidden)
     dx, dy = p2 .- p1
-    c_x = dx ≥ 0 ? '>' : '<'
-    c_y = dy ≥ 0 ? '^' : 'v'
-    return c_x^abs(dx) * c_y^abs(dy) * 'A'
-end
+    cx = dx_dict[sign(dx)]
+    cy = dy_dict[sign(dy)]
 
-function type(str, dict)
-    rev = Dict(val => key for (key, val) in dict)
-    pos = dict['A']
-    res = ""
-    for c in str
-        if haskey(dir_arr, c)
-            pos = pos .+ dir_arr[c]
-        else
-            res *= rev[pos]
-        end
+    b1 = best_paths[('A', cx)] + best_paths[(cx, cy)] + best_paths[(cy, 'A')]
+    b2 = best_paths[('A', cy)] + best_paths[(cy, cx)] + best_paths[(cx, 'A')]
+    presses = abs(dx) + abs(dy) - 2
+    if p1 .+ (dx, 0) == forbidden
+        return b2 + presses
+    elseif p1 .+ (0, dy) == forbidden
+        return b1 + presses
+    else
+        return min(b1, b2) + presses
     end
-    return res
+end
+shortest_arr(c1, c2, best_paths) = shortest(coords_arr[c1], coords_arr[c2], best_paths, (0, 1))
+shortest_num(c1, c2, best_paths) = shortest(coords_num[c1], coords_num[c2], best_paths, (0, 0))
+
+function update(best_paths)
+    return Dict((c1, c2) => shortest_arr(c1, c2, best_paths) for c1 in keys(coords_arr) for c2 in keys(coords_arr))
 end
 
-function advance(dict, (acc, pos), c)
-    new_pos = dict[c]
-    return (acc * move_to(pos, new_pos), new_pos)
-end
-advance_num(x...) = advance(coords_num, x...)
-advance_arr(x...) = advance(coords_arr, x...)
-
-function move_check(str)
-    res = move_arr(str)
-    check = type(res, coords_arr)
-    str != check && println("ERROR : ", res, " ", check, " ", str)
-    return res
-end
-
-check(s) = type(type(type(s, coords_arr), coords_arr), coords_num)
-
-move_num(str) = first(foldl(advance_num, str, init=("", coords_num['A'])))
-move_arr(str) = first(foldl(advance_arr, str, init=("", coords_arr['A'])))
-move = move_check ∘ move_check ∘ move_num
-
-score(str) = length(move(str)) * parse(Int, str[1:end-1])
+type(code, best_paths) = last(foldl(((key, acc), new_key) -> (new_key, acc + shortest_num(key, new_key, best_paths)), code, init=('A', 0)))
+score(code, best_paths) = type(code, best_paths) * parse(Int, code[1:end-1])
 
 open(ARGS[1]) do file
-    println(coords_num)
-    println(coords_arr)
-    # for line in readlines(file)
-    #     res = move(line)
-    #     println(check(res))
-    #     println(length(move(line)))
-    # end
-    println(sum(score, readlines(file)))
+    best_paths = Dict((c1, c2) => 1 for c1 in keys(coords_arr) for c2 in keys(coords_arr))
+    for _ in 1:25
+        best_paths = update(best_paths)
+    end
+
+    println(sum(x -> score(x, best_paths), readlines(file)))
 end
